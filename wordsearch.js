a1dd2d4fef6d25106f716171770f1e122e39f4f9
Roylu7384 <roylu7384@gmail.com>
2022-02-16 14:20:51 -0500

    complete assignment

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,33 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+const transpose = function(matrix) {
+  let neo = [];
+  if (matrix.length === 0) {
+    return [];
+  }
+  let length = matrix[0].length;
+  for (let i = 0; i < length; i++) {
+    let row = [];
+    for (let j = 0; j < matrix.length; j++) {
+      row.push(matrix[j].shift());
     }
-}
-
-module.exports = wordSearch
+    neo.push(row);
+  }
+  return neo;
+};
+  
+const wordSearch = (letters, word) => {
+  const horizontalJoin = letters.map(ls => ls.join(''));
+  for (const l of horizontalJoin) {
+    if (l.includes(word))
+      return true;
+  }
+  const vertical = transpose(letters);
+  const verticalJoin = vertical.map(vs => vs.join(''));
+  for (const v of verticalJoin) {
+    if (v.includes(word)) {
+      return true;
+    }
+  }
+  return false;
+};
+  
+module.exports = wordSearch;
